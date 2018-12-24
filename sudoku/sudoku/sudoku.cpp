905b610078985b0beb5546546e21370d8de055dc
fagen <33868268+fagen@users.noreply.github.com>
2018-12-24 21:25:47 +0800

    冗余行的删除，命令参数设置
    将文件分为多个，设置对命令行参数
    处理非法调用sudoku.exe的情况

--- sudoku/sudoku/sudoku.cpp
+++ sudoku/sudoku/sudoku.cpp
@@ -3,164 +3,11 @@
 ////fputs 将数独装换成一个长的字符串
 
 #include"generator.h"
+#include<iostream>
+using namespace std;
 static int num = 0;
 static int settle_flag = 0;
-//void sudoku_generate(int n)
-//{
-//	//char str[30];
-//	char line[9] = { '5','1','2','3','4','6','7','8','9' };
-//	char line1[19] = { '5',' ','1', ' ' ,'2', ' ','3',' ','4', ' ','6',' ','7',' ','8',' ','9','\n','\0' };
-//	int shift[9] = { 0,6,12,2,8,14,4,10,16 };
-//
-//	int pos1[6][3] = { { 3,4,5 },{ 3,5,4 },{ 4,5,3 },{ 4,3,5 },{ 5,4,3 },{ 5,3,4 } };
-//	int pos2[6][3] = { { 6,7,8 },{ 6,8,7 },{ 7,6,8 },{ 7,8,6 },{ 8,6,7 },{ 8,7,6 } };
-//
-//
-//	char final[10][19];
-//	int i, j, k;
-//	int flag = 0;
-//	char str[200];
-//
-//	for (int i = 0; i < 9; i++)//初始值置空格和\0
-//	{
-//		for (int j = 0; j < 17; j++)
-//		{
-//			final[i][j] = ' ';
-//		}
-//		final[i][17] = '\n';
-//		final[i][18] = '\0';
-//	}
-//	final[9][0] = '\n';//第10行只有一个空行
-//	final[9][1] = '\0';
-//
-//
-//	//freopen(SUDOKUPATH, "w", stdout);
-//	FILE *fp = fopen(GENERATEPATH, "w");
-//
-//	do//生成第一行
-//	{
-//		for (int i = 0; i < 9; i++)
-//		{
-//			line1[2 * i] = line[i];
-//		}
-//		memcpy(final[0], line1, sizeof(line1));
-//		for (i = 1; i < 9; i++)//以第一行为基础，生成一个终局
-//		{
-//			for (j = 0; j < 18; j += 2)
-//			{
-//				final[i][j] = line1[(j + shift[i]) % 18];
-//			}
-//		}
-//
-//		//在一个终局的基础上改变4-6,7-9行的输出顺序即可
-//		for (i = 0; i < 6; i++)
-//		{
-//			for (j = 0; j < 6; j++)
-//			{
-//				str[0] = '\0';
-//				flag++;
-//				for (k = 0; k < 3; k++)//前三行
-//				{
-//					//fputs(final[k], fp);
-//					strcat(str, final[k]);
-//				}
-//
-//				for (k = 0; k < 3; k++)//3 4 5行
-//				{
-//					//fputs(final[pos1[i][k]], fp);
-//					strcat(str, final[pos1[i][k]]);
-//				}
-//				//if (n > 1)
-//				//{
-//				for (k = 0; k < 3; k++)
-//				{
-//					//fputs(final[pos2[j][k]], fp);
-//					strcat(str, final[pos2[j][k]]);
-//				}
-//				//fputs(final[9], fp);//输出回车
-//				strcat(str, final[9]);
-//				if (n == 1)str[161] = '\0';
-//				fputs(str, fp);
-//				n--;
-//				if (!n) { fclose(fp); return; }
-//			}
-//		}
-//
-//	} while (next_permutation(line + 1, line + 9));
-//}
 
-
-//void ques_genetate(int ques_num)
-//{
-//
-//	FILE *fpQues1;
-//	FILE *fpBase1;
-//	char str[200];
-//	
-//	fpBase1 = fopen(BASEPATH,"r");
-//	fpQues1 = fopen(QUESPATH, "w");
-//	ques_board[9][0] = '\n';
-//	ques_board[9][1] = '\0';
-//	while (ques_num--)
-//	{
-//		str[0] = '\0';
-//		for (int i = 0; i < 9; i++)
-//		{
-//			fgets(ques_board[i], 20, fpBase1);
-//		}fgetc(fpBase1);
-//
-//		//int base[9] = { 0,3,6,27,30,33,54,57,60 };
-//		int base[9] = { 0,6,12,54,60,66,108,114,120 };
-//		//int plus[9] = { 0,1,2,9,10,11,18,19,20 };
-//		int plus[9] = { 0,2,4,18,20,22,36,38,40 };
-//
-//		for (int k = 0; k < 9; k++)//每个3*3随机掏空2个
-//		{
-//
-//			int i, j,
-//				hole[2];//3*3里面掏的位置
-//			hole[0] = rand() % 9;
-//			//hole[1] *= 2;
-//			hole[1] = rand() % 9;
-//			//hole[2] *= 2;
-//			while (hole[0] == hole[1])//防止重复
-//			{
-//				hole[1] = rand() % 9;
-//			}
-//			for (int t = 0; t < 2; t++)
-//			{
-//				int dot;
-//				dot = base[k] + plus[hole[t]];
-//				i = dot / 18;
-//				j = dot % 18;
-//				ques_board[i][j] = '0';
-//			}
-//		}
-//
-//		//已经掏空了18个
-//		int others;
-//		others = 12 + rand() % 31;//再掏12-41个就可以了
-//		while (others--)
-//		{
-//			int k = rand() % 81;
-//			int i = k / 9;
-//			int j = k % 9;
-//			
-//			j *= 2;
-//			if (ques_board[i][j] != '0')ques_board[i][j] = '0';
-//			else others++;
-//		}
-//
-//		//freopen(QUESPATH, "w", stdout);
-//		for (int i = 0; i < 10; i++)
-//		{
-//			strcat(str, ques_board[i]);
-//		}if (!ques_num) str[161] = '\0';
-//		fputs(str, fpQues1);
-//	}
-//	fclose(fpBase1);
-//	fclose(fpQues1);
-//}
 
 int check(int m, int n)
 {
@@ -186,7 +33,7 @@ int check(int m, int n)
 	else if (n / 6 == 1)j = 6;
 	else j = 12;
 
-	//int sum = 0;
+	//判断9个3*3是否满足
 	for (int c = i; c<i + 3; c++)
 		for (int d = j; d < j + 6; d+=2)
 		{
@@ -205,7 +52,7 @@ void prune(int i, int j, bool point[10])
 	{
 		if (ques_board[k][j] != '0'&&k != i)point[ques_board[k][j] - '0'] = true;
 	}
-	int m, n;
+	int m = 0, n = 0;
 	if (i / 3 == 0)m = 0;
 	else if (i / 3 == 1)m = 3;
 	else if (i / 3 == 2)m = 6;
@@ -270,7 +117,12 @@ void settle_ques()
 	FILE *fpQues, *fpSolution;
 	char strSolution[200];
 	int n = 0;
-	fpQues = fopen(QUESPATH, "r");
+	//if(argv[2]==NULL)
+	if(AbsolutePath[0]==0) 
+		fpQues = fopen(QUESPATH, "r");
+	else 
+		fpQues = fopen(AbsolutePath, "r");
+
 	fpSolution = fopen(SUDOKUPATH, "w");
 	int flag = 0;
 	while (1)
@@ -311,45 +163,87 @@ void settle_ques()
 
 int main(int argc, char** argv)
 {
-	int num=1000;
-	int begintime, endtime;
-	//scanf("%d", &num);
 
-	begintime = clock();
-	//sudoku_generate(num);
-	settle_ques();
-	//ques_genetate(num);
-	endtime = clock();	//计时结束
-	//freopen("CON", "w", stdout);
-	printf("total time: %d\n",total);
-	printf("\nRunning Time：%dms\n", endtime - begintime);
+	if (argc != 3)//参数不等于3个
+	{
+		cout << "Illegal paramater number\n"
+			 << "Usage:\n"
+			 << "      sudoku.exe -c number --> generate n sudoku finals. \n"
+			 << "      sudoku.exe -s path --> Read sudoku from file in the given path,and solve them.\n"
+			 << "      sudoku.exe -p number --> produce sudoku problem into the given path(prepare for the sudoku game)\n"
+			 << "      The charactre should be itself:such as C is not equal to c.\n";
+		return 1;
+	}
+
+	if (strcmp(argv[1], "-c") && strcmp(argv[1], "-s") && strcmp(argv[1],"-p"))
+	{
+		cout<< "Illegal paramater\n"
+			<< "The first parameter should be -c or -s or -p\n"
+			<< "-c means generating sudoku\n"
+			<< "-s meas solve the problem read from the file\n";
+		return 1;
+	}
+
+	if (!strcmp(argv[1], "-c"))//judge the second parameter when -c
+	{
+		int len = strlen(argv[2]);
+		for (int i = 0; i < len; i++)
+		{
+			if (argv[2][i] > '9' || argv[2][i] < '0')
+			{
+				cout << "Illegal paramater\n";
+				cout << "The second parameter should be a positive integer\n";
+				return 1;
+			}
+		}
+
+		int number = 0;
+		for (int i = 0; i < len; i++)
+		{
+			number = number * 10 + argv[2][i] - '0';
+		}
+
+		sudoku_generate(number);
+		cout << "The sudoku final generated is in the sudoku.txt\n"
+			<< "Have a check\n";
+		return 0;
+	}
+
+	if (!strcmp(argv[1], "-s"))
+	{
+		strcpy(AbsolutePath, argv[2]);
+		settle_ques();
+
+		cout << "The answer you need is in the sudoku.txt\n"
+			<< "Have a check\n";
+		return 0;
+	}
+
+	if (!strcmp(argv[1], "-p"))//default path ques.txt
+	{
+		int len = strlen(argv[2]);
+		for (int i = 0; i < len; i++)
+		{
+			if (argv[2][i] > '9' || argv[2][i] < '0')
+			{
+				cout << "Illegal paramater\n";
+				cout << "The second parameter should be a positive integer\n";
+				return 1;
+			}
+		}
+
+		int number = 0;
+		for (int i = 0; i < len; i++)
+		{
+			number = number * 10 + argv[2][i] - '0';
+		}
+
+		ques_generate(number);
+		cout << "The sudoku question generated is in the ques.txt\n"
+			<< "Have a check\n";
+		return 0;
+	}
 
 	system("pause");
 	return 0;
 }
-
-
-
-
-
-
-//#include<stdio.h>
-//#include<stdlib.h>
-//#include<string.h>
-//#include<iostream>
-//using namespace std;
-//
-//int main()
-//{
-//	
-//	int k = 0;
-//	FILE *fp;
-//	char str[10];
-//	char str2[10];
-//	fp = fopen("test.txt", "r");
-//	//fgets(str,10,fp);
-//	while (fgetc(fp))
-//		;
-//	system("pause");
-//	return 0;
-//}
